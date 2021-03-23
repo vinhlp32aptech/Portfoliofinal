e3216cded5aad4b2c096a70cdc2ef771bd5d8d16
vinhlp32aptech <vinh.lp32@aptechlearning.edu.vn>
2021-03-23 18:16:51 +0700

    set up many things

---
+++ PortfolioOnline/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioOnline.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("dashboard")]
+    [Route("admin/dashboard")]
+    public class DashBoardController : Controller
+    {
+        [Route("index")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioOnline/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioOnline.Controllers
+{
+    [Route("about")]
+    public class AboutController : Controller
+    {
+        [Route("index")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
